27c05bd1eb152e2f021721bf7b4029b18eda7d98
VipparlaSreeram <vipparlasreeram2003@hotmail.com>
2021-12-22 16:28:12 +0400

    updated

--- starter/webcrawler/src/main/java/com/udacity/webcrawler/profiler/ProfilingMethodInterceptor.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/profiler/ProfilingMethodInterceptor.java
@@ -6,67 +6,57 @@ import java.lang.reflect.Method;
 import java.time.Clock;
 import java.time.Duration;
 import java.time.Instant;
-import java.time.ZonedDateTime;
 import java.util.Objects;
 
 /**
  * A method interceptor that checks whether {@link Method}s are annotated with the {@link Profiled}
  * annotation. If they are, the method interceptor records how long the method invocation took.
- * The lessons called this an invocation handler, not a method interceptor.
  */
 final class ProfilingMethodInterceptor implements InvocationHandler {
 
   private final Clock clock;
-  private final ProfilingState profilingState;
-  private final Object delegate;
-
+  private final Object targetObject;
+  private final ProfilingState state;
 
   // TODO: You will need to add more instance fields and constructor arguments to this class.
-  ProfilingMethodInterceptor(Clock clock,
-                             Object delegate, //The wrap method called this delegate
-                             ProfilingState  profilingState) {
+  ProfilingMethodInterceptor(Clock clock, Object target, ProfilingState profilingState) {
     this.clock = Objects.requireNonNull(clock);
-    this.profilingState = profilingState;
-    this.delegate = delegate;
+    this.targetObject = Objects.requireNonNull(target);
+    this.state = Objects.requireNonNull(profilingState);
   }
-
-  /*
-  These verbose comments are for me (the student).  I am struggling to fully
-  understand proxy objects.
-
-  When a method in a proxy object is called, this is the function that handles
-  that method invocation.  The method that was called is passed in 'method' and
-  the arguments for that method live in the array called 'args'.
-   */
-  @Override
+    @Override
   public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
     // TODO: This method interceptor should inspect the called method to see if it is a profiled
     //       method. For profiled methods, the interceptor should record the start time, then
     //       invoke the method using the object that is being profiled. Finally, for profiled
     //       methods, the interceptor should record how long the method call took, using the
     //       ProfilingState methods.
+    final Instant start = clock.instant();
 
-    Object result = null;
-
-    if (method.isAnnotationPresent(Profiled.class)) {
-
-      final Instant startTime = clock.instant();
-
-  
+    if (method.getDeclaringClass().equals(Object.class)) {
       try {
-        result = method.invoke(delegate, args);
+        return method.invoke(targetObject, args);
       } catch (InvocationTargetException e) {
         throw e.getTargetException();
-      } catch (Exception e) { 
-        throw e;  
+      } catch (IllegalAccessException e) {
+        throw new RuntimeException(e);
       } finally {
-
-        profilingState.record(delegate.getClass(), method,
-                Duration.between(startTime, clock.instant()));
+        state.record(targetObject.getClass(), method, Duration.between(start, clock.instant()));
       }
+    }
 
+    if (method.getAnnotation(Profiled.class) != null) {
+      try {
+        method.invoke(targetObject, args);
+      } catch (InvocationTargetException e) {
+        throw e.getTargetException();
+      } catch (IllegalArgumentException e) {
+        throw new RuntimeException(e);
+      } finally {
+        state.record(targetObject.getClass(), method, Duration.between(start, clock.instant()));
+      }
+    }
 
-    } else result = method.invoke(delegate, args);
-    return result;
+    return method.invoke(targetObject, args);
   }
 }
